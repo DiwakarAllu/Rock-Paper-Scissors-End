6e5c0f237958a5476993a04d37262cde55aa3133
Allu Diwakar <101788832+DiwakarAllu@users.noreply.github.com>
2023-10-04 22:32:23 +0530

    updated version of the game with added features
    Keeping track of user and computer scores.
    Determining the winner of each round and displaying the result.
    Allowing the user to play again.

--- rock-paper-scissors-end.py
+++ rock-paper-scissors-end.py
@@ -1,5 +1,7 @@
+
 import random
 
+# Define the game images
 rock = '''
     _______
 ---'   ____)
@@ -26,31 +28,54 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-game_images = [rock, paper, scissors]
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_choice >= 3 or user_choice < 0: 
-    print("You typed an invalid number, you lose!") 
-else:
+game_images = [rock, paper, scissors]
+choices = ["Rock", "Paper", "Scissors"]
+
+# Initialize scores
+user_score = 0
+computer_score = 0
+
+# Function to determine the winner
+def determine_winner(user_choice, computer_choice):
+    global user_score, computer_score
+    if user_choice == computer_choice:
+        return "It's a draw!"
+    elif (
+        (user_choice == 0 and computer_choice == 2)
+        or (user_choice == 1 and computer_choice == 0)
+        or (user_choice == 2 and computer_choice == 1)
+    ):
+        user_score += 1
+        return "You win!"
+    else:
+        computer_score += 1
+        return "You lose!"
+
+# Main game loop
+while True:
+    print("Welcome to Rock, Paper, Scissors!")
+    user_choice = int(input("Type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
+    
+    if user_choice < 0 or user_choice > 2:
+        print("Invalid choice. Please choose again.")
+        continue
+
+    print("You chose:")
     print(game_images[user_choice])
 
     computer_choice = random.randint(0, 2)
     print("Computer chose:")
     print(game_images[computer_choice])
 
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
 
-    if user_choice == 0 and computer_choice == 2:
-        print("You win!")
-    elif computer_choice == 0 and user_choice == 2:
-        print("You lose")
-    elif computer_choice > user_choice:
-        print("You lose")
-    elif user_choice > computer_choice:
-        print("You win!")
-    elif computer_choice == user_choice:
-        print("It's a draw")
+    print(f"Your score: {user_score} | Computer's score: {computer_score}")
 
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        break
 
-
-
+print("Thanks for playing!")
 
